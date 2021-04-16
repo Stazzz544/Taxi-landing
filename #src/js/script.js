865cc3bc2cc9a91fa90fc74652041e10314cd83d
Stazzz <stazzzdzr@gmail.com>
2021-04-16 17:44:22 +0300

    3 sections are ready

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,3 +1,5 @@
+@@include('swiper-bundle.min.js');
+
 //функция для подключения webp
 function testWebP(callback) {
 
@@ -35,3 +37,39 @@ window.addEventListener('DOMContentLoaded', () => {
 		 })
 	})
 })
+
+
+var swiperMobile = new Swiper('.swiper-container-mobile', {
+	effect: 'coverflow',
+	grabCursor: true,
+	centeredSlides: true,
+	slidesPerView: 'auto',
+	// autoplay: {
+	// 	delay: 2500,
+	// 	disableOnInteraction: false,
+	//  },
+	coverflowEffect: {
+	  rotate: 50,
+	  stretch: 0,
+	  depth: 100,
+	  modifier: 1,
+	  slideShadows: true,
+	},
+	pagination: {
+	  el: '.swiper-pagination',
+	},
+	
+ });
+
+ var swiperUppertunity = new Swiper('.swiper-container-uppertunity', {
+	spaceBetween: 30,
+	centeredSlides: true,
+	// autoplay: {
+	//   delay: 2500,
+	//   disableOnInteraction: false,
+	// },
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev',
+	},
+ });
