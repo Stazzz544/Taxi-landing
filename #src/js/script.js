d90d4a1d22abb73d9736c21f38b1ca00052c2605
Stas_PC <stazzzdzr@gmail.com>
2021-04-18 23:55:04 +0300

    desctop version is ready

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,4 +1,6 @@
 @@include('swiper-bundle.min.js');
+@@include('jquery-1.12.4.min.js');
+@@include('jquery.tabs.js');
 
 //функция для подключения webp
 function testWebP(callback) {
@@ -29,25 +31,42 @@ window.addEventListener('DOMContentLoaded', () => {
 		 burger.classList.toggle('burger_active'); //класс куда тыкаем и что к нему добавиться через пробел (2)
 		 menu.classList.toggle('menu__items_active');  //тот самый класс, который добавился(1)
 	});
-
-	menuItem.forEach(item => {
-		 item.addEventListener('click', () => {
-			  burger.classList.toggle('.burger_active'); //класс куда тыкаем и что к нему добавиться через пробел
-			  menu.classList.toggle('.menu__items_active');//тот самый класс, который добавился(1)
-		 })
-	})
 })
+
+
+window.addEventListener('DOMContentLoaded', () => {
+	const sidebar = document.querySelector('.sidebar'), //класс, к которому добавится доп класс(1)
+	sidebarActivator = document.querySelector('.close__menu'); //класс куда тыкаем (2)
+	sidebarActivator2 = document.querySelector('.close__menu2'); //класс куда тыкаем (2)
+
+	
+
+	sidebarActivator2.addEventListener('click', () => {
+		sidebarActivator2.classList.toggle('close__menu2'); //класс куда тыкаем и что к нему добавиться через пробел (2)
+
+		 sidebar.classList.remove('sidebar_active');  //тот самый класс, который добавился(1)
+	})
+
+	sidebarActivator.addEventListener('click', () => {
+		sidebarActivator.classList.toggle('close__menu'); //класс куда тыкаем и что к нему добавиться через пробел (2)
+
+		 sidebar.classList.toggle('sidebar_active');  //тот самый класс, который добавился(1)
+	});
+
+})
+
 
 
 var swiperMobile = new Swiper('.swiper-container-mobile', {
 	effect: 'coverflow',
 	grabCursor: true,
 	centeredSlides: true,
+	loop: true,
 	slidesPerView: 'auto',
-	// autoplay: {
-	// 	delay: 2500,
-	// 	disableOnInteraction: false,
-	//  },
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	 },
 	coverflowEffect: {
 	  rotate: 50,
 	  stretch: 0,
@@ -63,13 +82,20 @@ var swiperMobile = new Swiper('.swiper-container-mobile', {
 
  var swiperUppertunity = new Swiper('.swiper-container-uppertunity', {
 	spaceBetween: 30,
+	loop: true,
 	centeredSlides: true,
-	// autoplay: {
-	//   delay: 2500,
-	//   disableOnInteraction: false,
-	// },
+	autoplay: {
+	  delay: 2500,
+	  disableOnInteraction: false,
+	},
 	navigation: {
 	  nextEl: '.swiper-button-next',
 	  prevEl: '.swiper-button-prev',
 	},
  });
+
+
+ //tabs
+ $(function () {
+	$('#horizontalTab').jqTabs({direction: 'horizontal'});
+});
