e00ca35af3c045d9e089e52c8f8b4e517322a045
Stas_PC <stazzzdzr@gmail.com>
2021-04-19 23:16:40 +0300

    mobile is ready

--- Taxi-landing-rus/js/script.js
+++ Taxi-landing-rus/js/script.js
@@ -85,33 +85,62 @@ testWebP(function (support) {
 
 window.addEventListener('DOMContentLoaded', () => {
 	const menu = document.querySelector('.menu__items'), //класс, к которому добавится доп класс(1)
-	burger = document.querySelector('.burger'); //класс куда тыкаем (2)
+		burger = document.querySelector('.burger'); //класс куда тыкаем (2)
 
 	burger.addEventListener('click', () => {
-		 burger.classList.toggle('burger_active'); //класс куда тыкаем и что к нему добавиться через пробел (2)
-		 menu.classList.toggle('menu__items_active');  //тот самый класс, который добавился(1)
+		burger.classList.toggle('burger_active'); //класс куда тыкаем и что к нему добавиться через пробел (2)
+		menu.classList.toggle('menu__items_active');  //тот самый класс, который добавился(1)
 	});
 })
 
 
+//sidebar
+
 window.addEventListener('DOMContentLoaded', () => {
 	const sidebar = document.querySelector('.sidebar'), //класс, к которому добавится доп класс(1)
-	sidebarActivator = document.querySelector('.close__menu'); //класс куда тыкаем (2)
+		sidebarActivator = document.querySelector('.close__menu'); //класс куда тыкаем (2)
 	sidebarActivator2 = document.querySelector('.close__menu2'); //класс куда тыкаем (2)
 
-	
-
-	sidebarActivator2.addEventListener('click', () => {
-		sidebarActivator2.classList.toggle('close__menu2'); //класс куда тыкаем и что к нему добавиться через пробел (2)
-
-		 sidebar.classList.remove('sidebar_active');  //тот самый класс, который добавился(1)
-	})
 
 	sidebarActivator.addEventListener('click', () => {
-		sidebarActivator.classList.toggle('close__menu'); //класс куда тыкаем и что к нему добавиться через пробел (2)
-
-		 sidebar.classList.toggle('sidebar_active');  //тот самый класс, который добавился(1)
+		sidebar.classList.toggle('sidebar_active');  //тот самый класс, который добавился(1)
 	});
+
+	sidebarActivator2.addEventListener('click', () => {
+		sidebar.classList.remove('sidebar_active');  //тот самый класс, который убираем(1)
+	})
+})
+
+//modal
+
+window.addEventListener('DOMContentLoaded', () => {
+	const modal = document.querySelector('.overlay');
+	const bodyLock = document.querySelector('body');
+	
+	modalActivator1 = document.querySelector('.modal-activator1');
+	modalActivator2 = document.querySelector('.modal-activator2');
+	modalActivator3 = document.querySelector('.modal-activator3');
+	modalRemover = document.querySelector('.modal__close');
+
+
+	modalActivator1.addEventListener('click', () => {
+		modal.classList.toggle('overlay_active');
+		bodyLock.classList.toggle('lock');
+	}),
+	modalActivator2.addEventListener('click', () => {
+		modal.classList.toggle('overlay_active');
+		bodyLock.classList.toggle('lock');
+	}),
+	modalActivator3.addEventListener('click', () => {
+		modal.classList.toggle('overlay_active');
+		bodyLock.classList.toggle('lock');
+	}),
+
+	modalRemover.addEventListener('click', () => {
+		modal.classList.remove('overlay_active');
+		bodyLock.classList.remove('lock');
+	});
+
 
 })
 
@@ -123,39 +152,80 @@ var swiperMobile = new Swiper('.swiper-container-mobile', {
 	centeredSlides: true,
 	loop: true,
 	slidesPerView: 'auto',
-	autoplay: {
-		delay: 2500,
-		disableOnInteraction: false,
-	 },
+	// autoplay: {
+	// 	delay: 2500,
+	// 	disableOnInteraction: false,
+	//  },
 	coverflowEffect: {
-	  rotate: 50,
-	  stretch: 0,
-	  depth: 100,
-	  modifier: 1,
-	  slideShadows: true,
+		rotate: 50,
+		stretch: 0,
+		depth: 100,
+		modifier: 1,
+		slideShadows: true,
 	},
 	pagination: {
-	  el: '.swiper-pagination',
+		el: '.swiper-pagination',
 	},
-	
- });
 
- var swiperUppertunity = new Swiper('.swiper-container-uppertunity', {
+});
+
+var swiperUppertunity = new Swiper('.swiper-container-uppertunity', {
 	spaceBetween: 30,
 	loop: true,
 	centeredSlides: true,
-	autoplay: {
-	  delay: 2500,
-	  disableOnInteraction: false,
-	},
+	// autoplay: {
+	//   delay: 2500,
+	//   disableOnInteraction: false,
+	// },
 	navigation: {
-	  nextEl: '.swiper-button-next',
-	  prevEl: '.swiper-button-prev',
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
 	},
- });
+});
 
 
- //tabs
- $(function () {
-	$('#horizontalTab').jqTabs({direction: 'horizontal'});
+//tabs
+$(function () {
+	$('#horizontalTab').jqTabs({ direction: 'horizontal' });
+});
+
+$('input[name=phone]').mask("+7 (999) 999-99-99");
+
+
+
+
+
+//modal
+$('[data-modal=consultation]').on('click', function(){
+	$('.overlay, #consultation').fadeIn('slow');
+}); 
+$('.modal__close').on('click', function(){
+	$('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+});
+
+$('.catalog-item__btn').each(function(i) {
+	$(this).on('click', function() {
+		$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+		$('.overlay, #order').fadeIn('slow');
+	})
+})
+
+
+
+
+//mail
+$('form').submit(function(e) {
+	 e.preventDefault();
+	 $.ajax({
+		  type: "POST",
+		  url: "mailer/smart.php",
+		  data: $(this).serialize()
+	 }).done(function() {
+		  $(this).find("input").val("");
+		  $('#consultation, #order').fadeOut();
+		  $('.overlay, #thanks').fadeIn('slow');
+
+		  $('form').trigger('reset');
+	 });
+	 return false;
 });
